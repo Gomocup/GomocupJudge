b05bc92430b17d31a9420de444f34c16086fdd59
Kai Sun <sunkaicn@gmail.com>
2017-04-21 21:18:53 -0400

    bug fix

--- client/utility.py
+++ client/utility.py
@@ -26,10 +26,11 @@ def str_to_pos(s):
 def psq_to_psq(_psq, board_size):
     psq = ''
     #psq += 'Piskvorky ' + str(board_size) + "x" + str(board_size) + "," + " 11:11," + " 0\n"
-    tt = 0
-    for x,y,t in _psq:
-        psq += str(x+1)+","+str(y+1)+","+str(t-tt)+"\n"
-        tt = t
+    tt = [0, 0]
+    for i in range(len(_psq)):
+        x,y,t = _psq[i]
+        psq += str(x+1)+","+str(y+1)+","+str(t-tt[i%2])+"\n"
+        tt[i%2] = t
     return psq
 
 def get_dir_size(start_path):
@@ -39,3 +40,7 @@ def get_dir_size(start_path):
             fp = os.path.join(dirpath, f)
             total_size += os.path.getsize(fp)
     return total_size
+
+
+if __name__ == '__main__':
+    print psq_to_psq([(18,12,36),(11,11,146), (18,10,72), (10,10,291)], 20)
