7adb97a931cdabec9483824b3640a1b87008c17c
Kai Sun <sunkaicn@gmail.com>
2020-04-01 02:48:19 -0700

    update

--- wrapper/client.py
+++ wrapper/client.py
@@ -6,15 +6,25 @@ import time
 from queue import Queue, Empty
 from threading import Thread
 
+global_timeout = 0.1
+global_retry = 10
+
 class client(object):
-    def __init__(self, host, port, ai):
+    def __init__(self, host, port, key, ai):
         self.host = host
         self.port = port
+        self.key = key
         self.ai = ai
         self.process = None
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect((self.host, self.port))
-
+        self.client_socket = None
+        self.msgqueue = []
+        self.msgstamp = int(str(time.time()).replace('.', ''))
+        self.msgok = set()
+        self.timestamp = {}
+        self.timestamp["send"] = time.time() - global_timeout
+        self.timestamp["ping"] = time.time() - global_timeout
+        self.timestamp["connected"] = time.time() - global_timeout
+        
     def _recv_socket(self, size):
         try:
             self.buf_socket
@@ -26,7 +36,7 @@ class client(object):
                 ret = self.buf_socket[:i]
                 self.buf_socket = self.buf_socket[i+1:]
                 return ret
-        self.client_socket.settimeout(0.01)
+        self.client_socket.settimeout(global_timeout)
         try:
             buf = self.client_socket.recv(size).decode()
             self.buf_socket = self.buf_socket + buf
@@ -43,16 +53,56 @@ class client(object):
             else:
                 return buf            
         return None
-    
+
+    def reconnect(self):
+        try:
+            if time.time() >= self.timestamp["ping"] + global_timeout:
+                self.client_socket.settimeout(global_timeout)
+                self.client_socket.send(("ping " + self.key + "\n").encode())
+                self.client_socket.settimeout(None)
+                self.timestamp["ping"] = time.time()
+            assert(time.time() < self.timestamp["connected"] + global_timeout * global_retry)
+        except:
+            while True:
+                #print("reconnecting...", flush=True)
+                try:
+                    self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    self.client_socket.connect((self.host, self.port))
+                    self.timestamp["connected"] = time.time()
+                    self.client_socket.settimeout(global_timeout)
+                    self.client_socket.send(("ping " + self.key + "\n").encode())
+                    self.client_socket.settimeout(None)
+                    break
+                except:
+                    time.sleep(global_timeout)
+                    
     def recv(self):
         while True:
-            ret = self._recv_socket(16*1024*1024)
-            if ret is not None:
-                return ["socket", ret]
-            ret = self._recv_ai()
-            if ret is not None:
-                return ["ai", ret]
-            time.sleep(0.01)
+            self.reconnect()
+            try:
+                ret = self._recv_socket(16*1024*1024)
+                if ret is not None:
+                    return ["socket", ret]
+                ret = self._recv_ai()
+                if ret is not None:
+                    return ["ai", ret]
+            except:
+                pass
+            time.sleep(global_timeout)
+
+    def send(self):
+        while len(self.msgqueue) > 0:
+            self.reconnect()
+            try:
+                print(str(time.time()) + "\t" + self.msgqueue[0][1].strip(), flush=True)
+                if time.time() >= self.timestamp["send"] + global_timeout:
+                    self.client_socket.settimeout(global_timeout)
+                    self.client_socket.send(("msg " + str(self.msgqueue[0][0]) + " " + self.msgqueue[0][1]).encode())
+                    self.client_socket.settimeout(None)
+                    self.timestamp["send"] = time.time()
+                break
+            except:
+                time.sleep(global_timeout)
 
     def write(self, msg):
         self.process.stdin.write(msg.encode())
@@ -61,38 +111,74 @@ class client(object):
     def listen(self):
         while True:
             buf = self.recv()
+            #print(buf, flush=True)
             if buf[0] == "socket":
-                if buf[1].startswith("new"):
-                    self.process = subprocess.Popen(shlex.split(self.ai),
-                                                    shell=False,
-                                                    stdin=subprocess.PIPE,
-                                                    stdout=subprocess.PIPE,
-                                                    bufsize=1,
-                                                    close_fds='posix' in sys.builtin_module_names,
-                                                    cwd=".")
-                    def enqueue_output(out, q):
-                        for line in iter(out.readline, b''):
-                            q.put(line)
-                        out.close()
-                    self.queue = Queue()
-                    queuethread = Thread(target=enqueue_output, args=(self.process.stdout, self.queue))
-                    queuethread.daemon = True
-                    queuethread.start()
+                if len(buf[1]) > 0:
+                    self.timestamp["connected"] = time.time()
+                if buf[1].startswith("ping"):
+                    pass
+                elif buf[1].startswith("new"):
+                    msgid = int(buf[1].split(" ")[1])
+                    if msgid not in self.msgok:
+                        self.msgok.add(msgid)
+                        if self.process is not None:
+                            self.process.kill()
+                        self.process = subprocess.Popen(shlex.split(self.ai),
+                                                        shell=False,
+                                                        stdin=subprocess.PIPE,
+                                                        stdout=subprocess.PIPE,
+                                                        bufsize=1,
+                                                        close_fds='posix' in sys.builtin_module_names,
+                                                        cwd=".")
+                        def enqueue_output(out, q):
+                            for line in iter(out.readline, b''):
+                                q.put(line)
+                            out.close()
+                        self.queue = Queue()
+                        queuethread = Thread(target=enqueue_output, args=(self.process.stdout, self.queue))
+                        queuethread.daemon = True
+                        queuethread.start()
+                    try:
+                        self.client_socket.settimeout(global_timeout)
+                        self.client_socket.send(("ok "+str(msgid)+"\n").encode())
+                        self.client_socket.settimeout(None)
+                    except:
+                        pass
                 elif buf[1].startswith("msg"):
-                    self.write(buf[1][4:]+"\n")
+                    msgid = int(buf[1].split(" ")[1])
+                    msg = ' '.join(buf[1].split(" ")[2:])
+                    if msgid not in self.msgok:
+                        self.msgok.add(msgid)
+                        self.write(msg+"\n")
+                        sys.stdout.flush()
+                    try:
+                        self.client_socket.settimeout(global_timeout)
+                        self.client_socket.send(("ok "+str(msgid)+"\n").encode())
+                        self.client_socket.settimeout(None)
+                    except:
+                        pass
+                elif buf[1].startswith("ok"):
+                    msgid = int(buf[1].strip().split(" ")[1])
+                    if len(self.msgqueue) > 0 and self.msgqueue[0][0] == msgid:
+                        del self.msgqueue[0]
+                        self.timestamp["send"] = time.time() - global_timeout
+                        
             elif buf[0] == "ai":
-                self.client_socket.send(buf[1].encode())
+                self.msgqueue += [(self.msgstamp, buf[1])]
+                self.msgstamp += 1
+            self.send()
 
 def main():
     parser = argparse.ArgumentParser(description='Gomocup Experimental Tournament Client')
 
     parser.add_argument("--host", dest="host", action="store", required=True)
     parser.add_argument("--port", dest="port", action="store", required=True)
+    parser.add_argument("--key", dest="key", action="store", required=True)
     parser.add_argument("--ai", dest="ai", action="store", required=True)
     
     args = parser.parse_args()
 
-    client(host=args.host, port=int(args.port), ai=args.ai).listen()
+    client(host=args.host, port=int(args.port), key=args.key, ai=args.ai).listen()
 
 
 if __name__ == '__main__':
--- wrapper/server.py
+++ wrapper/server.py
@@ -5,33 +5,42 @@ from threading import Thread
 from queue import Queue, Empty
 import time
 
+global_timeout = 0.1
+global_retry = 10
+
 class server(object):
-    def __init__(self, host, port):
+    def __init__(self, host, port, key):
         self.host = host
         self.port = port
+        self.key = key
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind((host, port))
+        self.conn = None
+        self.addr = None
         def enqueue_output(q):
             while True:
                 q.put(sys.stdin.readline())
             out.close()
         self.queue = Queue()
+        self.msgqueue = []
+        self.msgstamp = int(str(time.time()).replace('.', ''))
+        self.msgok = set()
+        self.timestamp = {}
+        self.timestamp["send"] = time.time() - global_timeout
+        self.timestamp["ping"] = time.time() - global_timeout
+        self.timestamp["connected"] = time.time() - global_timeout
         queuethread = Thread(target=enqueue_output, args=(self.queue,))
         queuethread.daemon = True
         queuethread.start()
 
     def _recv_socket(self, size):
-        try:
-            self.buf_socket
-        except:
-            self.buf_socket = ""
-
         for i in range(len(self.buf_socket)):
             if self.buf_socket[i] == "\n":
                 ret = self.buf_socket[:i]
                 self.buf_socket = self.buf_socket[i+1:]
                 return ret
-        self.conn.settimeout(0.01)
+        self.conn.settimeout(global_timeout)
         try:
             buf = self.conn.recv(size).decode()
             self.buf_socket = self.buf_socket + buf
@@ -47,39 +56,108 @@ class server(object):
         else:
             return buf
         return None
+
+    def reconnect(self):
+        try:
+            if time.time() >= self.timestamp["ping"] + global_timeout:
+                self.conn.settimeout(global_timeout)
+                self.conn.send(("ping\n").encode())
+                self.conn.settimeout(None)
+                self.timestamp["ping"] = time.time()
+            assert(time.time() < self.timestamp["connected"] + global_timeout * global_retry)
+        except:
+            while True:
+                try:
+                    self.server_socket.listen()
+                    self.conn, self.addr = self.server_socket.accept()
+                    #print(self.addr, flush=True)
+                    self.timestamp["connected"] = time.time()
+                    self.buf_socket = ""
+                    c = 0
+                    while c < global_retry:
+                        buf = self._recv_socket(16*1024*1024)
+                        if buf is not None:
+                            break
+                        time.sleep(global_timeout)
+                        c += 1
+                    if buf is not None and buf.startswith("ping") and buf[5:].strip() == self.key:
+                        break
+                except:
+                    pass
     
     def recv(self):
         while True:
-            ret = self._recv_socket(16*1024*1024)
-            if ret is not None:
-                return ["socket", ret]
-            ret = self._recv_stdin()
-            if ret is not None:
-                return ["stdin", ret]
-            time.sleep(0.01)
+            self.reconnect()
+            try:
+                ret = self._recv_socket(16*1024*1024)
+                if ret is not None:
+                    return ["socket", ret]
+                ret = self._recv_stdin()
+                if ret is not None:
+                    return ["stdin", ret]
+            except:
+                pass
+            time.sleep(global_timeout)
+
+    def send(self):
+        while len(self.msgqueue) > 0:
+            self.reconnect()
+            try:
+                if time.time() >= self.timestamp["send"] + global_timeout:
+                    self.conn.settimeout(global_timeout)
+                    self.conn.send((self.msgqueue[0][1]).encode())
+                    self.conn.settimeout(None)
+                    self.timestamp["send"] = time.time()
+                break
+            except:
+                time.sleep(global_timeout)
 
     def listen(self):
-        self.server_socket.listen()
-        self.conn, self.addr = self.server_socket.accept()
-        self.conn.send("new\n".encode())
+        self.msgqueue += [(self.msgstamp, "new " + str(self.msgstamp) + "\n")]
+        self.msgstamp += 1
         while True:
             buf = self.recv()
             if buf[0] == "socket":
-                sys.stdout.write(buf[1]+"\n")
-                sys.stdout.flush()
+                #print(buf, flush=True)
+                if len(buf[1]) > 0:
+                    self.timestamp["connected"] = time.time()
+                if buf[1].startswith("ping"):
+                    pass
+                elif buf[1].startswith("msg"):
+                    #print(buf, flush=True)
+                    msgid = int(buf[1].split(" ")[1])
+                    msg = ' '.join(buf[1].split(" ")[2:])
+                    if msgid not in self.msgok:
+                        self.msgok.add(msgid)
+                        sys.stdout.write(msg+"\n")
+                        sys.stdout.flush()
+                    try:
+                        self.conn.settimeout(global_timeout)
+                        self.conn.send(("ok "+str(msgid)+"\n").encode())
+                        self.conn.settimeout(None)
+                    except:
+                        pass
+                elif buf[1].startswith("ok"):
+                    msgid = int(buf[1].strip().split(" ")[1])
+                    if len(self.msgqueue) > 0 and self.msgqueue[0][0] == msgid:
+                        del self.msgqueue[0]
+                        self.timestamp["send"] = time.time() - global_timeout
             elif buf[0] == "stdin":
-                self.conn.send(("msg "+buf[1]).encode())
+                self.msgqueue += [(self.msgstamp, ("msg " + str(self.msgstamp) + " " + buf[1]))]
+                self.msgstamp += 1
+            self.send()
             
             
 def main():
-    parser = argparse.ArgumentParser(description='Gomocup Experimental Tournament Client')
+    parser = argparse.ArgumentParser(description='Gomocup Experimental Tournament Server')
 
     parser.add_argument("--host", dest="host", action="store", required=True)
     parser.add_argument("--port", dest="port", action="store", required=True)
+    parser.add_argument("--key", dest="key", action="store", required=True)
 
     args = parser.parse_args()
     
-    server(host=args.host, port=int(args.port)).listen()
+    server(host=args.host, port=int(args.port), key=args.key).listen()
             
 if __name__ == '__main__':
     main()
